eab2fdac62b634fe4da11ce384476f9d5bbead47
Nathan Daly <nhdaly@gmail.com>
2019-08-09 12:25:13 -0400

    Add docstring

--- src/ExitWithParent.jl
+++ src/ExitWithParent.jl
@@ -2,6 +2,14 @@ module ExitWithParent
 
 export exit_with_parent!
 
+"""
+    exit_with_parent!(p::Base.Process, signal = 2)
+
+When this julia process exits, the provided process `p` will be killed with `signal`.
+
+This spawns a child process that simply polls until the parent is killed, and then kills `p`.
+"""
+function exit_with_parent! end
 if Sys.isunix()
     function exit_with_parent!(p::Base.Process, signal = 2)  # SIGINT (ctrl-c)
         targetpid = getpid(p)
@@ -16,6 +24,7 @@ if Sys.isunix()
                 end
             end
         "`);
+        nothing
     end
 else
     # TODO: What should this do for other systems?
